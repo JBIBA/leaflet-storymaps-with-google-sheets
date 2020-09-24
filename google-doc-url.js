79f7600f7c6bc6505b0b6ffc777548bb837bc6c2
johanh0927 <59871531+johanh0927@users.noreply.github.com>
2020-09-24 12:48:59 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/188p0V5GBwzpKOK2cZvh0gvr1ACPW-rnYbY8jvrWLsK8/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyDwFpfS0bVDhelJ36JskKnbrgvFGvLiXyc';
